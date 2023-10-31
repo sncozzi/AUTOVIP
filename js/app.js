b5e3223b51aaeaa8913862e2ab3eb71f9c4f93c7
sncozzi <santiago.cozzi@anima.edu.uy>
2023-10-31 18:39:00 -0300

    js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const carImg = document.getElementById("carImg");
+const year = document.getElementById("year");
+const price = document.getElementById("price");
+const description = document.getElementById("description")
+const brand = document.getElementById("brand")
+
+fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
+.then(function (res){
+    return res.json();
+})
+.then
