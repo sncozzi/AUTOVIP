c24e7916185b2a6f4043c7ed806e5c330acccf76
bahiyyihteamo <luana.cruz@anima.edu.uy>
2023-10-31 21:05:10 -0300

    years

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 const carImg = document.getElementById("carImg");
 const year = document.getElementById("year");
 const price = document.getElementById("price");
-const description = document.getElementById("description")
-const brand = document.getElementById("brand")
+const description = document.getElementById("description");
+const brand = document.getElementById("brand");
 
-fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
-.then(function (res){
-    return res.json();
-})
-.then
-
-.catch(function (err) {
-    console.error(err);
-    loader.style.display = "none"; })
+fetch("https://ha-front-api-proyecto-final.vercel.app/cars").then(function (
+  res
+) {
+  return res.json();
+}).then;
+for (let i = 1900; i < 2023; i++) {
+  let option = document.createElement("option");
+  option.value = i;
+  option.textContent = i;
+  year.appendChild(option);
+}
